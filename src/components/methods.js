f081a121ba92e9206d8fbc7ead6deb8b0d66e43a
Zachery Converse <zacheryconverse@gmail.com>
2021-04-29 15:35:47 -0400

    clean up methods.js

--- src/components/methods.js
+++ src/components/methods.js
@@ -7,7 +7,6 @@ const zacheryToken = process.env.REACT_APP_ZACHERY_TOKEN;
 const codyToken = process.env.REACT_APP_CODY_TOKEN;
 // // const secret = process.env.REACT_APP_SECRET;
 const userID = "Zachery";
-// const channelName = "!members-_fyCsJ3J7Mc51W36qIE6HlC8zrpvIAq_DIJSkongGUY";
 
 const client = StreamChat.getInstance(apiKey);
 
@@ -23,54 +22,6 @@ const getChannels = async () => {
   const response = await client.queryChannels(filter);
   console.log(response[0]?.id, "response", response);
 };
-
-//     // console.log(channel);
-
-//     const deleteChannel = async () => {
-//       console.log(zacheryToken);
-//       await client.connectUser({ id: userID }, zacheryToken).catch((error) => console.log('connect', error));
-//       const channel = client.channel("messaging", channelName);
-//       await channel.watch().catch((error) => console.log("watch"));;
-//       const destroy = await channel
-//         .delete()
-//         .catch((error) => console.log("delete"));;
-//       console.log(destroy, "DESTROY");
-//     }
-
-//     const test = async () => {
-//       const response = await deleteChannel();
-//       // const response = await getChannels();
-//   console.log(response, 'response');
-// };
-
-// test();
-
-// require("dotenv").config({ path: __dirname + "/.env" });
-// const { createToken } = require("../../server-side/createToken");
-
-// const StreamChat = require("stream-chat").StreamChat;
-// const apiKey = process.env["API_KEY"];
-
-// For client-side auth the client uses only the app_key
-// const client = StreamChat.getInstance(apiKey);
-
-// const userId = "Zachery";
-// const extraData = {
-//   image: '',
-// };
-
-// Fetch token from client-side method to create token
-// const token = createToken(userId);
-
-// const connect = client.connectUser({ id: userId, extraData }, token);
-// client.connectUser({ id: userID }, zacheryToken);
-
-// const cid = "!members-_fyCsJ3J7Mc51W36qIE6HlC8zrpvIAq_DIJSkongGUY";
-// const channel = client.channel("messaging", cid);
-// await channel.create();
-
-// return await channel.watch();
-// fetch the channel state, subscribe to future updates
 
 // const text = "@Zack Hello there from Steve";
 
