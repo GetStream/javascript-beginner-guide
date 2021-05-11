52489ce7922045276fd6679631370a025015e822
Cody Smith <monstaro32@gmail.com>
2021-05-11 11:43:46 -0600

    Remove chatClient instantiation and move it to ContextProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { StreamChat } from "stream-chat";
 import Login from "./components/Login";
 import UserList from "./components/UserList";
 import ChannelList from "./components/ChannelList";
@@ -7,44 +6,28 @@ import OneOnOne from "./components/OneOnOne";
 import Lobby from "./components/Lobby";
 import Navigation from "./components/Navigation";
 import "./App.css";
-
-// The key (and secret) exist in the Dashboard
-// Dashboard - https://getstream.io/accounts/login/
-const appKey = process.env["REACT_APP_KEY"];
+import { ChatClientProvider } from "./ChatClientContext";
 
 export default function App() {
   const [view, setView] = useState("login");
   const [channel, setChannel] = useState(null);
 
-  // Instantiate client on client-side with app key
-  //   https://getstream.io/chat/docs/javascript/?language=javascript
-  const chatClient = StreamChat.getInstance(appKey);
   return (
-    <div className="App">
-      {view === "login" ? (
-        <Login chatClient={chatClient} setView={setView} />
-      ) : view === "users" ? (
-        <UserList
-          chatClient={chatClient}
-          setChannel={setChannel}
-          setView={setView}
-        />
-      ) : view === "contacts" ? (
-        <ChannelList
-          chatClient={chatClient}
-          setChannel={setChannel}
-          setView={setView}
-        />
-      ) : view === "lobby" ? (
-        <Lobby
-          chatClient={chatClient}
-          setChannel={setChannel}
-          setView={setView}
-        />
-      ) : (
-        <OneOnOne chatClient={chatClient} channel={channel} view={view} />
-      )}
-      <Navigation chatClient={chatClient} setView={setView} view={view} />
-    </div>
+    <ChatClientProvider>
+      <div className="App">
+        {view === "login" ? (
+          <Login setView={setView} />
+        ) : view === "users" ? (
+          <UserList setChannel={setChannel} setView={setView} />
+        ) : view === "contacts" ? (
+          <ChannelList setChannel={setChannel} setView={setView} />
+        ) : view === "lobby" ? (
+          <Lobby setChannel={setChannel} setView={setView} />
+        ) : (
+          <OneOnOne channel={channel} view={view} />
+        )}
+        <Navigation setView={setView} view={view} />
+      </div>
+    </ChatClientProvider>
   );
 }
